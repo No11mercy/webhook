ac78970634a55903b87c994a7e50e8341579603f
No11mercy <2018103084@pnu.edu.ru>
2024-08-16 19:34:19 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import requests
 
 app = Flask(__name__)
 
@@ -7,16 +8,18 @@ LOCAL_SERVER_URL = 'http://127.0.0.1:5000/webhook'
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    data = request.json
-    
-    # Передача данных на локальный сервер
     try:
+        data = request.json
+        
+        # Передача данных на локальный сервер
         response = requests.post(LOCAL_SERVER_URL, json=data)
         response.raise_for_status()  # Проверка на ошибки HTTP
+        
+        return jsonify({'message': 'Webhook received and forwarded'}), 200
     except requests.RequestException as e:
-        return jsonify({'error': str(e)}), 500
-
-    return jsonify({'message': 'Webhook received and forwarded'}), 200
+        return jsonify({'error': f'Request error: {str(e)}'}), 500
+    except Exception as e:
+        return jsonify({'error': f'Internal server error: {str(e)}'}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)  # Настройка сервера Flask на прослушивание всех IP-адресов
+    app.run(host='0.0.0.0', port=5000)
